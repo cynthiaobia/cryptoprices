029732e460089d8e6ec55b955ad9e68f2207beb4
Cynthia <cynthia.obia@gmail.com>
2023-08-07 13:36:15 -0500

    first commit

---
+++ src/pages/Price.js
@@ -0,0 +1,52 @@
+
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function Price (props) {
+
+  const [coin, setCoin] = useState(null);
+
+  const params = useParams(); 
+  const { symbol } = params;
+
+  const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${process.env.REACT_APP_COINAPI_KEY}`;
+
+  const getCoin = async() => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+    setCoin(data);
+    } catch (e) {
+      console.log('ERROR FETCHING DATA ', e)
+    }
+  }
+
+  // runs as soon as the component mounts
+  useEffect(() => {
+    getCoin();
+  }, []);
+
+  // show the fetched data
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+
+        <h2>
+          {coin.rate}
+        </h2>
+      </div>
+    )
+  }
+
+  // show a loading message
+  const loading = () => <h1>Loading...</h1>
+
+  return (
+    coin && coin.rate ? loaded() : loading()
+  )
+}
+
+export default Price;
